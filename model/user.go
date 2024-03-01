298ca6e07f2ea05fc013b01398bedace9c37d762
riny <riny@aspecta.id>
2024-03-01 17:42:11 +0800

    :memo: add user table to token table ORM

--- model/user.go
+++ model/user.go
@@ -11,20 +11,18 @@ type User struct {
 	Email      string `json:"email" gorm:"column:email" gorm:"unique"`
 	Password   string `json:"password" gorm:"column:password"`
 	VerifyCode string `json:"verify_code" gorm:"column:verify_code"`
-}
 
-func (User) TableName() string {
-	return "main_user"
+	Token Token `json:"token,omitempty" gorm:"foreignKey:UserId;references:Id"`
 }
 
 type Token struct {
 	gorm.Model
 	Id     int32     `json:"id" gorm:"column:id" gorm:"primaryKey"`
-	UserId int32     `json:"user_id" gorm:"column:user_id" gorm:"foreignKey:Id"`
+	UserId int32     `json:"user_id" gorm:"column:user_id" gorm:"unique"`
 	Token  string    `json:"token" gorm:"column:token"`
 	InDate time.Time `json:"in_date" gorm:"column:in_date"`
 }
 
-func (Token) TableName() string {
-	return "main_token"
-}
+func (User) TableName() string { return "main_user" }
+
+func (Token) TableName() string { return "main_token" }
